f1b166630e9933df59d948b79431b72dd7ce9e5d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-07 01:53:38 -0500

    Address code rabbits concerns.

--- src/Components/Tabs/Tabs.js
+++ src/Components/Tabs/Tabs.js
@@ -8,10 +8,10 @@ import "./Tabs.scss";
  * Renders the tabs.
  * @return {JSX.Element}
  */
-export function Tabs ({}) {
+export function Tabs () {
     return (
         <div className="tabs d-flex">
-            <Tab tabName={"sample.py"}></Tab>;
+            <Tab tabName={"sample.py"}></Tab>
         </div>
     );
 }
