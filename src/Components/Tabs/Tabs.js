a800c5d9b64313775e2b6210ee41e03af9412169
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-07 01:43:12 -0500

    Add tab and tabs components.

---
+++ src/Components/Tabs/Tabs.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {Tab} from "./Tab/Tab";
+
+import "./Tabs.scss";
+
+/**
+ * Renders the tabs.
+ * @return {JSX.Element}
+ */
+export function Tabs ({}) {
+    return (
+        <div className="tabs d-flex">
+            <Tab tabName={"sample.py"}></Tab>;
+        </div>
+    );
+}
