01d5feab8083dbf488113b76aff2ae98a87f68b2
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-16 11:54:16 -0400

    Add initial implementation of websocket client.

--- src/Providers/CDLProviders.js
+++ src/Providers/CDLProviders.js
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import PropTypes from "prop-types";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
 CDLProviders.propTypes = {
     children: PropTypes.object,
 };
+
+const WS_URL = "ws://localhost:8765";
 
 /**
  * Provides all contexts consumed by the application.
@@ -13,7 +16,52 @@ CDLProviders.propTypes = {
  * @return {JSX}
  */
 function CDLProviders ({children}) {
-    const [isLoading, setIsLoading] = useState(false);
+    // State that holds the history of recived messages
+    const [messageHistory, setMessageHistory] = useState([]);
+
+    // Open websocket connection and reconnect when it closes
+    const {sendJsonMessage, lastMessage, readyState} = useWebSocket(
+        WS_URL,
+        {
+            share: false,
+            shouldReconnect: () => true,
+        }
+    );
+
+    // React to changes in the websocket ready state
+    useEffect(() => {
+        console.debug(`Connection state: ${connectionStatus}`);
+        switch (readyState) {
+            case ReadyState.OPEN:
+                sendJsonMessage({event: "connected"});
+                break;
+            case ReadyState.CONNECTING:
+                break;
+            case ReadyState.CLOSING:
+                break;
+            case ReadyState.CLOSED:
+                break;
+            case ReadyState.UNINSTANTIATED:
+                break;
+        }
+    }, [readyState]);
+
+    // Map connection status to string for debugging
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: "Connecting",
+        [ReadyState.OPEN]: "Open",
+        [ReadyState.CLOSING]: "Closing",
+        [ReadyState.CLOSED]: "Closed",
+        [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+    }[readyState];
+
+    // React to received messages
+    useEffect(() => {
+        if (lastMessage) {
+            console.debug(`Received Message: ${lastMessage.data}`);
+            setMessageHistory((prev) => prev.concat(lastMessage));
+        }
+    }, [lastMessage]);
 
     return (
         <>
