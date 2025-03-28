7409ac0d49e1e90d18393dc061918158d4514d81
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-27 20:17:12 -0400

    Add support for connecting to ASP server.

---
+++ src/Providers/FileTreeContext.js
@@ -0,0 +1,7 @@
+import {createContext} from "react";
+
+// This context stores the file tree which has the source
+// code for each of the files in the program.
+const FileTreeContext = createContext();
+
+export default FileTreeContext;
