09d1ffeadd077bb58f616c3db2192cb59636bfbc
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-25 08:34:47 -0400

    Add filter container.

---
+++ src/Components/SystemSelectorContainer/Filter/Filter.js
@@ -0,0 +1,15 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import "./Filter.scss";
+
+/**
+ * Contains the component used to filter through system level traces.
+ * @return {JSX.Element}
+ */
+export function Filter () {
+    return (
+        <div style={{height: "100px", width: "100%"}}>
+        
+        </div>
+    );
+}
