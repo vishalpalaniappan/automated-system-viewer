4c2ee4f39dae507fc589d1b3fe1b5fe5d8b40b25
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-05 18:55:16 -0400

    Use correct system version key when building system id.

--- src/Providers/System.js
+++ src/Providers/System.js
@@ -7,13 +7,15 @@ class System {
      * @param {Object} systemInfo Information about the loaded system.
      */
     constructor (systemInfo) {
-        this.id = systemInfo.system_id + "_" + systemInfo.version;
+        this.id = systemInfo.system_id + "_" + systemInfo.system_ver;
         this.sysId = String(systemInfo.system_id);
         this.version = String(systemInfo.system_ver);
         this.programs = systemInfo.programs;
         this.deployments = systemInfo.deployments;
-
-        console.log("Created System:", this.sysId, this.version);
+        console.log("");
+        console.log("Created System");
+        console.log("System ID:", this.sysId);
+        console.log("System Version:", this.version);
     }
 }
 
