fb1a3de513b206699ca6f232f905cdb697ca33ed
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-12 00:13:12 -0400

    Load filterable values from key.

--- src/Components/SystemSelectorContainer/EditFilter/EditFilter.js
+++ src/Components/SystemSelectorContainer/EditFilter/EditFilter.js
@@ -19,6 +19,8 @@ import "./EditFilter.scss"
 export function EditFilter ({show, handleClose, title, body}) {
     const {activeTraces, setActiveTraces} = useContext(ActiveTracesContext);
     const [filterableValues, setFilterableValues] = useState();
+    const [selectedKey, setSelectedKey] = useState();
+    const [selectedKeyValues, setSelectedKeyValues] = useState();
 
     useEffect(() => {
         if (activeTraces) {
@@ -28,15 +30,63 @@ export function EditFilter ({show, handleClose, title, body}) {
         }
     }, [activeTraces]);
 
+    const getKeys = () => {
+        if (filterableValues?.keys) {
+            const _keys = [];
+            filterableValues.keys.forEach((key, index) => {
+                _keys.push(<option key={key} value={key}>{key}</option>);
+            });
+            return _keys;
+        }
+    };
+
+    const getValues = () => {
+        if (filterableValues?.values) {
+            console.log(filterableValues.values[selectedKey]);
+        }
+    };
+
+    useEffect(() => {
+        if (selectedKey && filterableValues?.values) {
+            console.log("Selected Key:", selectedKey);
+            setSelectedKeyValues(filterableValues.values[selectedKey]);
+        }
+    }, [selectedKey]);
+
 
     return (
-        <Modal show={show} onHide={handleClose} centered data-bs-theme="dark">
-            <Modal.Header>
-                Edit Filter
-            </Modal.Header>
+        <Modal show={show} onHide={handleClose} size="lg" centered data-bs-theme="dark">
             <Modal.Body>
 
+                <div className="d-flex flex-row">
+                    <label style={{width: "100px"}}>Select Key:</label>
+                    <select onChange={(e) => {setSelectedKey(e.target.value);}}
+                        value={selectedKey} className="d-flex customSelector">
+                        {getKeys()}
+                    </select>
+                </div>
 
+                <div className="d-flex flex-row" style={{"height": "400px"}}>
+                    <div className="h-100 d-flex flex-grow-1">
+                        <div className="w-100 h-100 p-3">
+                            <div className="selectContainer">
+                                {getValues()}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="h-100" style={{"width": "50px"}}>
+
+                    </div>
+
+                    <div className="h-100 d-flex flex-grow-1">
+                        <div className="w-100 h-100 p-3">
+                            <div className="selectContainer">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Modal.Body>
         </Modal>
     );
